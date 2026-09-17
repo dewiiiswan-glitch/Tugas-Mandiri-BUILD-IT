254fd2bc359ce1c2ee423e817fad84217f71d85d
unknown <dewiiiswan@gmail.com>
2026-09-17 22:44:23 +0800

    menambahkan stdlib.h di soal_3.c

--- soal_3.c
+++ soal_3.c
@@ -1,37 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
 int main() {
-    int a, b, c;
-    int ronde = 0;
+    int n;
+    scanf("%d", &n);
+    long long *a = malloc(n * sizeof(long long));
 
-    scanf("%d %d %d", &a, &b, &c);
-
-    while (a != b && a != c && b != c) {
-
-        // Pemain paling sedikit mendapat 1 kartu
-        if (a < b && a < c)
-            a++;
-        else if (b < a && b < c)
-            b++;
-        else
-            c++;
-
-        // Pemain paling banyak memberi 1 kartu
-        if (a > b && a > c)
-            a--;
-        else if (b > a && b > c)
-            b--;
-        else
-            c--;
-
-        ronde++;
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &a[i]);
     }
 
-    printf("%d", ronde);
+    qsort(a, n, sizeof(long long), bandingkan);
+
+    long long total = (1LL * n * (n - 1) / 2) % MOD;
+
+    long long sama = 0;
+
+    int i = 0;
+
+    while (i < n) {
+        int j = i;
+
+        while (j < n && a[j] == a[i]) {
+            j++;
+        }
+
+        long long banyak = j - i;
+
+        sama = (sama + banyak * (banyak - 1) / 2) % MOD;
+
+        i = j;
+    }
+
+    long long jawaban = (total - sama + MOD) % MOD;
+
+    printf("%lld\n", jawaban);
+
+    free(a);
 
     return 0;
 }
-
